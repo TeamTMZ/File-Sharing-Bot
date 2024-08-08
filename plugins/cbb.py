1523df24611f941a6ea2c2c3cb59309ee6e9f973
TeamTMZ <165527852+TeamTMZ@users.noreply.github.com>
2024-08-08 21:20:45 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id=1572929036'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://graph.org/file/ca261a0b430f5a1eb51f2.jpg'>Click here</a>\n○ Channel : @TamilMoviesz_HD\n○ Support Group : @MoviezRequestTamil</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id=1572929036'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://graph.org/file/ca261a0b430f5a1eb51f2.jpg'>Click here</a>\n○ Channel : @TamilMoviesz_HD\n○ Support Group : @Tmz_support</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
